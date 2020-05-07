684dfeb46e78ec492dbe24e522e8996f08b2e446
Manish <manishdubey81189@gmail.com>
2020-05-07 13:38:01 +0530

    Day 7 : Cousins In Binary Tree : Binary tree structure added in question

--- src/week1/CousinsInBinaryTree.kt
+++ src/week1/CousinsInBinaryTree.kt
@@ -17,6 +17,12 @@
     Input: root = [1,2,3,null,4,null,5], x = 5, y = 4
     Output: true
 
+            1
+          /   \
+         2     3
+          \     \
+           4     5
+
     Example 3:
 
     Input: root = [1,2,3,null,4], x = 2, y = 3
@@ -26,8 +32,6 @@
 
     The number of nodes in the tree will be between 2 and 100.
     Each node has a unique integer value from 1 to 100.
-
-    1,2,3,null,4,null,5
 
  */
 
